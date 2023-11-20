b3d8076982ed23fbfb63b41463db25d294537840
mifrok <mifra@mail.ru>
2023-11-20 11:51:08 +0700

    added javascript + bootstrap

---
+++ students/NeshcheretEA/js/script.js
@@ -0,0 +1,93 @@
+window.onload = function () {
+
+    let a = ''
+    let b = ''
+    let expressionResult = ''
+    let selectedOperation = null
+
+// окно вывода результата
+    outputElement = document.getElementById("result")
+
+// список объектов кнопок циферблата (id которых начинается с btn_digit_)
+    digitButtons = document.querySelectorAll('[id ^= "btn_digit_"]')
+
+    function onDigitButtonClicked(digit) {
+        if (!selectedOperation) {
+            if ((digit != '.') || (digit == '.' && !a.includes(digit))) {
+                a += digit
+            }
+            outputElement.innerHTML = a
+        } else {
+            if ((digit != '.') || (digit == '.' && !b.includes(digit))) {
+                b += digit
+                outputElement.innerHTML = a + selectedOperation + b
+            }
+        }
+    }
+
+// устанавка колбек-функций на кнопки циферблата по событию нажатия
+    digitButtons.forEach(button => {
+        button.onclick = function () {
+            const digitValue = button.innerHTML
+            onDigitButtonClicked(digitValue)
+        }
+    });
+
+// установка колбек-функций для кнопок операций
+    document.getElementById("btn_op_mult").onclick = function () {
+        if (a === '') return
+        selectedOperation = 'x'
+        outputElement.innerHTML = a + selectedOperation
+    }
+    document.getElementById("btn_op_plus").onclick = function () {
+        if (a === '') return
+        selectedOperation = '+'
+        outputElement.innerHTML = a + selectedOperation
+    }
+    document.getElementById("btn_op_minus").onclick = function () {
+        if (a === '') return
+        selectedOperation = '-'
+        outputElement.innerHTML = a + selectedOperation
+    }
+    document.getElementById("btn_op_div").onclick = function () {
+        if (a === '') return
+        selectedOperation = '/'
+        outputElement.innerHTML = a + selectedOperation
+    }
+
+// кнопка очищения
+    document.getElementById("btn_op_clear").onclick = function () {
+        a = ''
+        b = ''
+        selectedOperation = ''
+        expressionResult = ''
+        outputElement.innerHTML = 0
+    }
+
+// кнопка расчёта результата
+    document.getElementById("btn_op_equal").onclick = function () {
+        if (a === '' || b === '' || !selectedOperation)
+            return
+
+        switch (selectedOperation) {
+            case 'x':
+                expressionResult = (+a) * (+b)
+                break;
+            case '+':
+                expressionResult = (+a) + (+b)
+                break;
+            case '-':
+                expressionResult = (+a) - (+b)
+                break;
+            case '/':
+                expressionResult = (+a) / (+b)
+                break;
+        }
+
+        a = expressionResult.toString()
+        b = ''
+        selectedOperation = null
+
+        outputElement.innerHTML = a
+    }
+};
